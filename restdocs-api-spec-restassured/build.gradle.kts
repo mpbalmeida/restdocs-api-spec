77c11a3d265963a7dbbb0695f23b1527cd2a5e52
marcos.almeida <me@marcosalmeida.dev>
2022-11-06 19:43:15 +0100

    chore: adjust contributor

--- restdocs-api-spec-restassured/build.gradle.kts
+++ restdocs-api-spec-restassured/build.gradle.kts
@@ -32,24 +32,24 @@ publishing {
             pom {
                 name.set("REST Doc API Spec - REST Assured")
                 description.set("Adds API specification support to Spring REST Docs ")
-                url.set("https://github.com/ePages-de/restdocs-api-spec")
+                url.set("https://github.com/mpbalmeida/restdocs-api-spec")
                 licenses {
                     license {
                         name.set("MIT License")
-                        url.set("https://github.com/ePages-de/restdocs-api-spec/blob/master/LICENSE")
+                        url.set("https://github.com/mpbalmeida/restdocs-api-spec/blob/master/LICENSE")
                     }
                 }
                 developers {
                     developer {
-                        id.set("ePages")
-                        name.set("ePages Devs")
-                        email.set("info@epages.com")
+                        id.set("marcos.almeida")
+                        name.set("Marcos Almeida")
+                        email.set("me@marcosalmeida.dev")
                     }
                 }
                 scm {
-                    connection.set("scm:git:git://github.com/ePages-de/restdocs-api-spec.git")
-                    developerConnection.set("scm:git:ssh://github.com/ePages-de/restdocs-api-spec.git")
-                    url.set("https://github.com/ePages-de/restdocs-api-spec")
+                    connection.set("scm:git:git://github.com/mpblamieda/restdocs-api-spec.git")
+                    developerConnection.set("scm:git:ssh://github.com/mpbalmeida/restdocs-api-spec.git")
+                    url.set("https://github.com/mpbalmeida/restdocs-api-spec")
                 }
             }
         }
